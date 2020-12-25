9cab842febe07cc0247113df8a4a8798bae5c6b8
natos <natan.osnis@gmail.com>
2020-12-25 18:54:11 +0200

    Removed FlashOnly

--- FlashOnly/Application/SensorController/scif_osal_tirtos.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-Application/SensorController/scif_osal_tirtos.obj: ../Application/SensorController/scif_osal_tirtos.c
-
-../Application/SensorController/scif_osal_tirtos.c: 
--- FlashOnly/Application/scif_osal_tirtos.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-Application/scif_osal_tirtos.obj: ../Application/scif_osal_tirtos.c
-
-../Application/scif_osal_tirtos.c: 
